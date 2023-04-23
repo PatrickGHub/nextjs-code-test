e6f332ee7a61ce1abe7c1f60173b913166b630eb
PatrickGHub <patrick@pkelly.co>
2023-04-23 22:10:11 +1000

    Add test endpoint logic and data

--- pages/api/integrations/index.ts
+++ pages/api/integrations/index.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import data from '../../../data.json'
 
 type Data = {
   name: string;
@@ -8,4 +9,12 @@ type Data = {
 export default function handler(
   _req: NextApiRequest,
   res: NextApiResponse<Data>
-) {}
+) {
+  if (_req.method === 'GET') {
+    return res.status(200).send(data)
+  }
+
+  if (_req.method === 'POST') {
+    return res.status(200).send({name: 'abc'})
+  }
+}
