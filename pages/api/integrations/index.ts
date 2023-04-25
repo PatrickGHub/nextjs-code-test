7c1144fe30142dce2919458dcb0c946874a7b532
PatrickGHub <patrick@pkelly.co>
2023-04-25 10:35:17 +1000

    Change type of integrations api response

--- pages/api/integrations/index.ts
+++ pages/api/integrations/index.ts
@@ -1,14 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { IntegrationsData } from "@/types";
 import data from '@/data'
-
-type Data = {
-  // name: string;
-};
 
 export default function handler(
   _req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<IntegrationsData>
 ) {
   if (_req.method === 'GET') {
     return res.status(200).send(data)
